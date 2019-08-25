19ed16a16b7a1215d3731877cdeb818dc67c7e9d
Unknown <abuyoyo@gitpush.com>
2019-08-25 22:03:33 +0300

    v0.2

--- vendor-autoload.php
+++ vendor-autoload.php
@@ -1,12 +1,19 @@
 <?php
 /**
  * Plugin Name: Vendor Autoload
- * Description: Use Composer's autoload
- * Version: 0.1
+ * Description: Include Composer autoload.php (Wordpress mu-plugin)
+ * Version: 0.2
  * Author: abuyoyo
  * Author URI: https://github.com/abuyoyo/
- * Last Update: 2019_04_21
+ * Last Update: 2019_08_25
  */
- 
- 
- include_once WP_CONTENT_DIR . '/vendor/autoload.php';
+
+if ( ! $vendor_dir = get_option( 'vendor_dir' ) ){
+	$vendor_dir = exec('composer config vendor-dir --absolute');
+	if ($vendor_dir)
+		add_option( 'vendor_dir', $vendor_dir, '', true );
+	else
+		return;
+}
+
+include_once $vendor_dir . '/autoload.php';
