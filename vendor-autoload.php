318f5171c1ffcaa9f5a41b81cad996fc2520ec92
Unknown <abuyoyo@gitpush.com>
2019-08-25 22:03:03 +0300

    v0.1

---
+++ vendor-autoload.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Vendor Autoload
+ * Description: Use Composer's autoload
+ * Version: 0.1
+ * Author: abuyoyo
+ * Author URI: https://github.com/abuyoyo/
+ * Last Update: 2019_04_21
+ */
+ 
+ 
+ include_once WP_CONTENT_DIR . '/vendor/autoload.php';
