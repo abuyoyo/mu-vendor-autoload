7a1fc87a35ad057548e31e4c4cc6febdaa1ad756
abuyoyo <abuyoyo@users.noreply.github.com>
2020-02-10 18:45:06 +0200

    v0.5 - simpler

--- vendor-autoload.php
+++ vendor-autoload.php
@@ -1,34 +1,17 @@
 <?php
 /**
- * Plugin Name: Vendor Autoload
- * Description: Include Composer autoload.php (Wordpress mu-plugin)
- * Version: 0.4
+ * Plugin Name: Vendor Autoload (MU)
+ * Description: Require vendor/autoload
+ * Version: 0.5
  * Author: abuyoyo
  * Author URI: https://github.com/abuyoyo/
- * Last Update: 2019_09_07
+ * Plugin URI: https://github.com/abuyoyo/mu-vendor-autoload
+ * Last Update: 2020_02_10
  */
+if ( ! defined( 'ABSPATH' ) )
+	die( 'No soup for you!' );
 
-/**
- * @todo this can be achieved with a post-install composer script
- * 		No need to access database
- * 		No need to run exec('composer')
- */
+if ( ! defined('VENDOR_DIR') )
+	define( 'VENDOR_DIR', WP_CONTENT_DIR . '/vendor' );
 
-if ( isset( $_REQUEST['reset_vendor_dir'] ) ){
-	delete_option( 'vendor_dir' );
-}
-
-if ( ! $vendor_dir = get_option( 'vendor_dir' ) ){
-	$vendor_dir = exec('composer config vendor-dir --absolute');
-	if ($vendor_dir)
-		add_option( 'vendor_dir', $vendor_dir, '', true );
-	else
-		return;
-}
-
-include_once $vendor_dir . '/autoload.php';
-
-// other mu-plugins that rely on $vendor_dir can hook here
-do_action('vendor_autoload');
-// redundancy
-do_action('vendor_dir');
+require VENDOR_DIR . '/autoload.php';
